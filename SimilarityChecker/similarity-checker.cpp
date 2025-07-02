6732896cf0ddc4f250d79af4515a83c7d83f0e0a
MJ <julysky40@gmail.com>
2025-07-02 15:31:28 +0900

    [feature] implement 60 and 0 score case of checkLength

--- SimilarityChecker/similarity-checker.cpp
+++ SimilarityChecker/similarity-checker.cpp
@@ -1,4 +1,12 @@
+#include <string>
+using std::string;
+
+
 class SimilarityChecker {
 public:
-
+	int checkLength(const string& a, const string& b) {
+		if (a.size() >= b.size() * 2) return 0;
+		if (b.size() >= a.size() * 2) return 0;
+		return 60;
+	}
 };
--- SimilarityChecker/similarity-checker_test.cpp
+++ SimilarityChecker/similarity-checker_test.cpp
@@ -1,9 +1,23 @@
 #include "gmock/gmock.h"
 #include "similarity-checker.cpp"
 
-TEST(SimilarityChecker, TC1) {
-	EXPECT_EQ(1, 1);
+TEST(SimilarityChecker, SameLengthStringTest) {
+	SimilarityChecker sc;
+	int expected = 60;
+	int actual = sc.checkLength("abc", "efg");
+	EXPECT_EQ(expected, actual);
 }
+
+TEST(SimilarityChecker, MoreThanTwiceStringTest) {
+	SimilarityChecker sc;
+	int expected = 0;
+	int actual = sc.checkLength("abcefgh", "efg");
+	EXPECT_EQ(expected, actual);
+	actual = sc.checkLength("efg", "abcefgh");
+	EXPECT_EQ(expected, actual);
+}
+
+
 
 int main() {
 	testing::InitGoogleMock();
